ee1daee933c67eb783cc1517b781ad0df0d84ef8
rikuyaMaster <108648258+rikuyaMaster@users.noreply.github.com>
2022-07-05 11:00:00 +0900

    edit App.jsx UseStateについて学びました

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
   // ボタンを押されたら
-  const onClickButton = () => alert();
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  // 第一引数：変数名、第二引数：関数名
+  // この状態だとnumに0が代入されている
+  const [num, setNum] = useState(0);
   return (
     <>
       <h1>皆さん初めまして</h1>
       {/* Proposの記載方法(値の受け渡し) */}
       <ColorfulMessage color="blue" message="お元気ですか?" />
       <ColorfulMessage color="pink" message="元気です" />
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
